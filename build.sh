3d5c9872d6474abc077f8e0e0143831a19e7677b
thomaskhub <48279513+thomaskhub@users.noreply.github.com>
2024-02-06 07:39:26 +0530

    Create build.sh
    Added boilerplate things need to be given some life next

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+# compile the package into wasm and create a dist directory
+# which will contain all output files to run wasm in the browser
+# This has been tested on ubuntu 22.04 but should run any many other 
+# Linux distributions as well
+echo "prepare distribution directory"
+
+echo "compile go package to wasm"
+
+echo "copy rhe go wasm runtime script"
+
+echo "copy java script wrapper to dist directory"
+
+echo "packaging done"
